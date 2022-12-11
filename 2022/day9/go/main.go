a1bb17e6d0c8d294b75dfd6a214ec62321e0ae6c
Rendom <a@vvt.nu>
2022-12-11 19:27:26 +0100

    Part 2

--- 2022/day9/go/main.go
+++ 2022/day9/go/main.go
@@ -20,7 +20,7 @@ type State struct {
 	Visited map[Pos]int // y, x, count
 
 	Head Pos
-	Tail Pos
+	Body []*Pos
 }
 
 func (s *Pos) Move(dir string, steps int) {
@@ -36,42 +36,48 @@ func (s *Pos) Move(dir string, steps int) {
 	}
 }
 
-func isTouching(a Pos, b Pos) bool {
-	cx := math.Abs(float64(a.x - b.x))
-	cy := math.Abs(float64(a.y - b.y))
-
-	if cx <= 1 && cy <= 1 {
-		return true
+func n(n int) int {
+	if n < 0 {
+		return -1
+	} else if n > 0 {
+		return 1
 	}
 
-	return false
+	return 0
 }
 
 func (s *State) Move(dir string, steps int) {
+	tailIdx := len(s.Body) - 1
 	for i := 1; i <= steps; i++ {
 		s.Head.Move(dir, 1)
-		// s.Visited[s.Head]++
-		if !isTouching(s.Head, s.Tail) {
-			if s.Head.x > s.Tail.x {
-				s.Tail.x++
-			} else if s.Head.x < s.Tail.x {
-				s.Tail.x--
+		follow := s.Head
+		for idx, part := range s.Body {
+			dx := follow.x - part.x
+			dy := follow.y - part.y
+
+			if math.Abs(float64(dx)) > 1 || math.Abs(float64(dy)) > 1 {
+				part.x += n(dx)
+				part.y += n(dy)
 			}
 
-			if s.Head.y > s.Tail.y {
-				s.Tail.y++
-			} else if s.Head.y < s.Tail.y {
-				s.Tail.y--
+			if idx == tailIdx {
+				s.Visited[*part]++
 			}
-			s.Visited[s.Tail]++
+
+			follow = *part
 		}
 	}
 }
 
-func parse(data string) (State, error) {
+func doStuff(data string, size int) (State, error) {
+	body := []*Pos{}
+	for i := 0; i < size; i++ {
+		body = append(body, &Pos{0, 0})
+	}
+
 	s := State{
 		Head:    Pos{0, 0},
-		Tail:    Pos{0, 0},
+		Body:    body,
 		Visited: map[Pos]int{},
 	}
 
@@ -95,7 +101,7 @@ func parse(data string) (State, error) {
 	return s, nil
 }
 
-func answer1(s State) int {
+func (s *State) calcVisited() int {
 	sum := 0
 	for _, v := range s.Visited {
 		if v > 0 {
@@ -106,12 +112,27 @@ func answer1(s State) int {
 	return sum
 }
 
-func main() {
-	state, err := parse(input)
+func answer1() {
+	s, err := doStuff(input, 1)
 	if err != nil {
 		fmt.Printf("Unable to parse, %v\n", err)
 		return
 	}
 
-	fmt.Printf("Answer 1: %d\n", answer1(state))
+	fmt.Printf("Answer 1: %d\n", s.calcVisited())
+}
+
+func answer2() {
+	s, err := doStuff(input, 9)
+	if err != nil {
+		fmt.Printf("Unable to parse, %v\n", err)
+		return
+	}
+
+	fmt.Printf("Answer 2: %d\n", s.calcVisited())
+}
+
+func main() {
+	answer1()
+	answer2()
 }
