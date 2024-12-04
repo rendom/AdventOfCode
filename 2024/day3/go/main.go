dccc97f5024f0e63132f7e9b13043d9b649bf8d0
Rendom <a@vvt.nu>
2024-12-04 10:04:10 +0100

    Day 3 part 2

--- 2024/day3/go/main.go
+++ 2024/day3/go/main.go
@@ -17,8 +17,56 @@ func main() {
 	fmt.Printf("Answer part 2: %d\n", answerPart2(input))
 }
 
+func parseVal(n string) ([]int, error) {
+	numbers := []int{}
+	for _, v := range strings.Split(n, ",") {
+		number, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+
+		numbers = append(numbers, number)
+	}
+
+	return numbers, nil
+}
+
+func parseOps(input string) ([][]int, error) {
+	r, err := regexp.Compile(`don't\(\)|do\(\)|mul\([0-9,]+\)`)
+	if err != nil {
+		return nil, err
+	}
+
+	m := r.FindAllString(input, -1)
+
+	result := [][]int{}
+	add := true
+	for _, m := range m {
+		if len(m) < 2 {
+			return nil, errors.New("Hm.. regex failed")
+		}
+
+		switch {
+		case m[:3] == "mul":
+			var n1 int = 1
+			var n2 int = 1
+			fmt.Sscanf(m, "mul(%d,%d)", &n1, &n2)
+
+			if add {
+				result = append(result, []int{n1, n2})
+			}
+		case m[:3] == "do(":
+			add = true
+		case m[:5] == "don't":
+			add = false
+		}
+	}
+
+	return result, nil
+}
+
 func parseMul(input string) ([][]int, error) {
-	r, err := regexp.Compile("mul\\(([0-9]+,[0-9]+)\\)")
+	r, err := regexp.Compile(`mul\(([0-9]+,[0-9]+)\)`)
 	if err != nil {
 		return nil, err
 	}
@@ -70,5 +118,10 @@ func answerPart1(input string) int {
 }
 
 func answerPart2(input string) int {
-	return 0
+	list, err := parseOps(input)
+	if err != nil {
+		panic(err)
+	}
+
+	return calcAnswer1(list)
 }
--- 2024/day3/go/main_test.go
+++ 2024/day3/go/main_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 const example string = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+const example_2 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
 func TestAnswerPart1(t *testing.T) {
 	answerPart1(example)
@@ -15,8 +16,8 @@ func TestAnswerPart1(t *testing.T) {
 }
 
 func TestAnswerPart2(t *testing.T) {
-	answer := 0
-	if r := answerPart2(example); r != answer {
+	answer := 48
+	if r := answerPart2(example_2); r != answer {
 		t.Fatalf("Wrong answer, got %d expected %d", r, answer)
 	}
 }
