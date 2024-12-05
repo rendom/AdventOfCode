ab216761dcd620e80eb558e6e31002ba2c2a3dbd
Rendom <a@vvt.nu>
2024-12-05 22:38:30 +0100

    D5P2

--- 2024/day5/go/main.go
+++ 2024/day5/go/main.go
@@ -12,7 +12,7 @@ import (
 var input string
 
 func parse(input string) (rules map[[2]int]bool, updates [][]int) {
-	// input = strings.TrimRight(input, "\n")
+	input = strings.TrimRight(input, "\n")
 	sections := strings.Split(input, "\n\n")
 	rules = make(map[[2]int]bool)
 
@@ -29,13 +29,9 @@ func parse(input string) (rules map[[2]int]bool, updates [][]int) {
 		}
 
 		rules[[2]int{before, after}] = true
-		fmt.Printf("%d|%d\n", before, after)
 	}
 
 	for _, r := range strings.Split(sections[1], "\n") {
-		if r == "" {
-			continue
-		}
 		pages := []int{}
 		for _, number := range strings.Split(r, ",") {
 			n, err := strconv.Atoi(number)
@@ -55,30 +51,44 @@ func main() {
 	fmt.Printf("Answer part 2: %d\n", answerPart2(input))
 }
 
-func answerPart1(input string) int {
+func solve(input string, partOne bool) int {
 	rules, updates := parse(input)
-	fmt.Println(len(rules), len(updates))
-
 	result := 0
 	for _, pages := range updates {
-		modified := slices.IsSortedFunc(pages, func(a int, b int) int {
-			if _, ok := rules[[2]int{a, b}]; ok {
-				return -1
-			} else if _, ok := rules[[2]int{b, a}]; ok {
-				return 1
-			}
-
-			return 0
-		})
-
-		if modified {
-			result += pages[len(pages)/2]
+		if value, alreadySorted := checkRow(pages, rules); alreadySorted == partOne {
+			result += value
 		}
 	}
 
 	return result
 }
 
+func answerPart1(input string) int {
+	return solve(input, true)
+}
+
+func checkRow(pages []int, rules map[[2]int]bool) (int, bool) {
+	fn := func(a int, b int) int {
+		if _, ok := rules[[2]int{a, b}]; ok {
+			return -1
+		} else if _, ok := rules[[2]int{b, a}]; ok {
+			return 1
+		}
+
+		return 0
+	}
+
+	alreadySorted := slices.IsSortedFunc(pages, fn)
+
+	if alreadySorted {
+		return pages[len(pages)/2], true
+	}
+
+	slices.SortFunc(pages, fn)
+	return pages[len(pages)/2], false
+}
+
 func answerPart2(input string) int {
-	return 0
+	return solve(input, false)
+
 }
